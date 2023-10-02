78bb4cee0eba805229279c355ee77de3e43795bf
Kathelin <kathelinishii@outlook.com>
2023-10-02 17:04:00 +0100

    Create script.sql

---
+++ ManipulandoDados/script.sql
@@ -0,0 +1,20 @@
+USE [VENDAS SUCOS]
+GO
+
+INSERT INTO [dbo].[PRODUTOS]
+           ([ID_PRODUTOS]
+           ,[DESCRITOR]
+           ,[SABOR]
+           ,[TAMANHO]
+           ,[EMBALAGEM]
+           ,[PRECO_LISTA])
+     VALUES
+           ('104113'
+           ,'Light - 350 ml - Limão'
+           ,'Limão'
+           ,'350 ml'
+           ,'Lata'
+           ,3.25)
+GO
+
+
