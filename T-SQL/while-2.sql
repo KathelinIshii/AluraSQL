dcefb9ef14d2b3111d00523e437dae2175123c41
Kathelin <kathelinishii@outlook.com>
2023-10-20 22:19:57 +0100

    Update while-2.sql

--- T-SQL/while-2.sql
+++ T-SQL/while-2.sql
@@ -1,11 +1,3 @@
---Sabendo que a função abaixo soma um dia a uma data:
-
---SELECT DATEADD(DAY, 1, @DATA)
-
---Como podemos fazer um script que, a partir do dia 01/01/2017,
---conte o número de notas fiscais até o dia 10/01/2017 e, além disso,
---imprima a data e o número de notas fiscais?
-
 SELECT * FROM [NOTAS FISCAIS];
 
 SELECT
@@ -15,15 +7,22 @@ FROM [NOTAS FISCAIS]
 
 DECLARE 
 	@DATA_INICIAL DATE,
-	@DATA_FINAL DATE;
+	@DATA_FINAL DATE,
+	@NUMERO FLOAT;
 
 SET @DATA_INICIAL = '2017-01-01';
-SET @DATA_FINAL = '2017-01-10'
+SET @DATA_FINAL = '2017-01-10';
+
+SELECT
+	@NUMERO = COUNT(NUMERO)
+FROM [NOTAS FISCAIS]
 
 PRINT 'ENTRANDO';
-WHILE @DATA_INICIAL <= @DATA_FINAL
+WHILE @DATA_INICIAL <= @DATA_FINAL 
 	BEGIN
 		PRINT @DATA_INICIAL
+		SELECT @NUMERO = COUNT(NUMERO) FROM [NOTAS FISCAIS] WHERE DATA = @DATA_INICIAL
+		PRINT @NUMERO
 		SELECT @DATA_INICIAL = DATEADD(DAY, 1, @DATA_INICIAL)
 	END;
 PRINT 'SAINDO';
